da023053a6ffe9889c3328f43fa67740de9d1294
fariedabuzaid <fariedaz@gmail.com>
2022-12-12 15:59:36 +0100

    add jupyter black to presentation requirements

--- build_scripts/install-presentation-requirements.sh
+++ build_scripts/install-presentation-requirements.sh
@@ -40,6 +40,8 @@ BUILD_DIR=$(dirname "$0")
   conda install -y -c conda-forge notebook rise jupyter_contrib_nbextensions
   #python build_scripts/configure_spellcheck_dict.py
   #jupyter nbextension enable spellchecker/main
+  jupyter  nbextension install https://github.com/drillan/jupyter-black/archive/master.zip --user
+  jupyter nbextension enable jupyter-black-master/jupyter-black
   jupyter nbextension enable equation-numbering/main
 )
 
