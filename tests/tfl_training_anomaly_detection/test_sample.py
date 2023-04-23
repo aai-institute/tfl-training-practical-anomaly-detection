b3f67c2633f43c2c120bf8c4ed97fd833ec59d40
fariedabuzaid <fariedaz@gmail.com>
2023-04-23 22:58:33 +0200

    Add dummy test

---
+++ tests/tfl_training_anomaly_detection/test_sample.py
@@ -0,0 +1,4 @@
+# the suggested naming convention for unit tests is test_method_name_testDescriptionInCamelCase
+# this leads to a nicely readable output of pytest
+def test_sample_class_attributes_greeterSaysHello():
+    assert True
