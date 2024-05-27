a7599e92889bb6974084433d11f8d59d92b52d2c
turnmanh <17703667+turnmanh@users.noreply.github.com>
2024-05-27 09:34:51 +0200

    moved to pip, dropping conda

--- build_scripts/install_presentation_requirements.sh
+++ build_scripts/install_presentation_requirements.sh
@@ -37,8 +37,7 @@ BUILD_DIR=$(dirname "$0")
 
 (
   cd "${BUILD_DIR}/.." || (echo "Unknown error, could not find directory ${BUILD_DIR}" && exit 255)
-  conda install -y -c conda-forge notebook==6.5.6 jupyter sphinx sphinx_rtd_theme jupyter-book jupyter_contrib_nbextensions jupyter_nbextensions_configurator
-  pip install rise==5.7.1
+  pip install notebook==6.5.6 rise==5.7.1 jupyter sphinx sphinx_rtd_theme jupyter-book jupyter_contrib_nbextensions jupyter_nbextensions_configurator
   python build_scripts/configure_spellcheck_dict.py
   jupyter contrib nbextension install --user
   jupyter nbextensions_configurator enable --user
